39dacfa60bd51ef8d25ec3b77573d4827a37f481
H Math <humilimath@gmail.com>
2015-02-20 15:25:09 +1100

    Filled out code for skeleton methods

--- cachematrix.R
+++ cachematrix.R
@@ -4,7 +4,19 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+     mat <- x
+     inverse <- NULL
+     get <- function() x
+     set <- function(y = matrix()) {
+          x <<- y
+          inverse <<- NULL
+     }
+     getInverse <- function()inverse
+     setInverse <- function(inv = matrix()){
+          inverse <<- inv
+     }
+     list(get = get, set = set, getInverse = getInverse, setInverse = setInverse)
+     
 }
 
 
@@ -12,4 +24,11 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+        if(!is.null(x$getInverse())){
+             message("Getting cached Data")
+             return(x$getInverse())
+        }
+        inv <- solve(x$get())
+        x$setInverse(inv)
+        inv
 }
